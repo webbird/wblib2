07e70e78ecbe7dedf791327995404f7066d94ad6
Bianka Martinovic <github.20.cerberus@spamgourmet.com>
2013-08-01 17:29:52 +0200

    added Lang class, debug subdir, 3rdparty subdir

---
+++ wblib/wbLang.php
@@ -0,0 +1,527 @@
+<?php
+
+/**
+ *
+ *          _     _  _ _
+ *         | |   | |(_) |
+ *    _ _ _| |__ | | _| |__
+ *   | | | |  _ \| || |  _ \
+ *   | | | | |_) ) || | |_) )
+ *   \___/|____/ \_)_|____/
+ *
+ *
+ *   @category     wblib
+ *   @package      wbLang
+ *   @author       BlackBird Webprogrammierung
+ *   @copyright    (c) 2013 BlackBird Webprogrammierung
+ *   @license      GNU LESSER GENERAL PUBLIC LICENSE Version 3
+ *
+ **/
+
+namespace wblib;
+
+/**
+ * language (internationalization) handling class
+ *
+ * @category   wblib
+ * @package    wbLang
+ * @copyright  Copyright (c) 2013 BlackBird Webprogrammierung
+ * @license    GNU LESSER GENERAL PUBLIC LICENSE Version 3
+ */
+if ( ! class_exists( 'wbLang', false ) )
+{
+    class wbLang {
+
+        /**
+         * array of instances (one instance per language)
+         **/
+        public static  $instances  = array();
+        /**
+         * logger
+         **/
+        private static $analog     = NULL;
+        /**
+         * log level
+         **/
+        public  static $loglevel    = 3;
+        /**
+         * array of default options
+         **/
+        public  static $defaults    = array( 'default' => 'EN', 'paths' => array(), 'case_insensitive' => false );
+        /**
+         * currently used language (for static use)
+         **/
+        private static $_current    = 'EN';
+        /**
+         * list of files already loaded (don't load twice)
+         **/
+        private static $_loaded     = array();
+        /**
+         * available language strings
+         **/
+        private static $_strings    = array();
+        /**
+         * available language strings, keys converted to lowercase
+         **/
+        private static $_strings_lo = array();
+
+        // private to make sure that constructor can only be called
+        // using getInstance()
+        private function __construct() {
+            if ( func_num_args() )
+                $this->args = func_get_args();
+        }    // end function __construct()
+
+        // no cloning!
+        private function __clone() {}
+
+        public function __call($method, $args)
+        {
+            if ( ! isset($this) || ! is_object($this) )
+                return false;
+            if ( method_exists( $this, $method ) )
+                return call_user_func_array(array($this, $method), $args);
+        }   // end function __call()
+
+        /**
+         * Create an instance
+         * @access public
+         * @param  array   $options    - OPTIONAL
+         * @return object
+         **/
+        public static function getInstance()
+        {
+            $language = call_user_func_array('self::getLang',func_get_args());
+            if ( !array_key_exists( $language, self::$instances ) ) {
+                self::log(sprintf('creating new instance with name [%s]',$language),7);
+                self::$instances[$language] = new self(func_get_args());
+                self::$_current             = $language;
+                // add local path to array
+                self::addPath(dirname(__FILE__).'/languages');
+                self::addFile($language);
+            }
+            return self::$instances[$language];
+        }   // end function getInstance()
+
+        /**
+         * change current language; creates a new instance if the given language
+         * is not loaded yet
+         *
+         * @access public
+         * @param  string  $lang
+         * @return
+         **/
+        public static function set($lang)
+        {
+            return self::getInstance($lang);
+        }   // end function set()
+        
+        /**
+         * get current language shortcut
+         *
+         * @access public
+         * @return string
+         *
+         **/
+        public function current()
+        {
+            return self::$_current;
+        } // end function current()
+
+        /**
+         * "translate" (try to find the given message in the language array)
+         *
+         * Will return the original string (but with placeholders replaced) if
+         * the string is not found in language array.
+         *
+         * @access public
+         * @param  string   $msg  - message to search for
+         * @param  array    $attr - attributes to replace in string
+         * @return string
+         **/
+        public static function translate($msg,$attr = array())
+        {
+            if ( empty( $msg ) || is_bool( $msg ) )
+                return $msg;
+            // direct match
+            if ( array_key_exists($msg, self::$_strings) )
+                $msg = self::$_strings[$msg];
+            // case insensitive match
+            if ( self::$defaults['case_insensitive'] && array_key_exists($msg, self::$_strings_lo) )
+                $msg = self::$_strings_lo[$msg];
+            preg_match_all( '~\[\[\s*?{{\s*?([^}].+?)\s*?}}\s*?\|\s*?([^\|].+?)\s*?\|\s*?([^\]].+?)\]\]~i', $msg, $matches, PREG_SET_ORDER );
+            foreach ( $matches as $match )
+            {
+                if ( isset($attr[$match[1]]) )
+                {
+                    $num    = $attr[$match[1]];
+                    $string = ( (int)$num > 1 || (int)$num == 0 )
+                            ? $match[3]
+                            : $match[2];
+                    $msg = str_replace($match[0], $num.' '.$string, $msg);
+                }
+                else
+                {
+                    $msg = str_replace($match[0], '0 '.$match[3], $msg);
+                }
+            }
+            // replace attributes
+            foreach ( $attr as $key => $value )
+                $msg = preg_replace( "~{{\s*$key\s*}}~i", $value, $msg );
+
+            return $msg;
+        } // end function translate()
+
+        /**
+         * convenience function; shortcut to translate()
+         **/
+        public static function lang($msg,$attr = array())
+        {
+            return self::translate($msg,$attr);
+        }   // end function lang()
+
+        /**
+         * convenience function; shortcut to translate()
+         **/
+        public static function t($msg,$attr = array())
+        {
+            return self::translate($msg,$attr);
+        }   // end function t()
+
+        /**
+         *
+         * @access public
+         * @return
+         **/
+        public static function getLang()
+        {
+            if ( func_num_args() )
+                $args = func_get_args();
+            self::log('args: '.var_export($args,1),7);
+            if ( isset($args[0]) )
+            {
+                if ( ! is_array($args[0]) )
+                    $language = array_shift($args);
+                elseif( isset($args['lang']) )
+                    $language = $args['lang'];
+            }
+            if ( !$language )
+                $language = self::getfrombrowser();
+            if ( !$language )
+                $language = self::$defaults['default'];
+            return $language;
+        }   // end function getLang()
+        
+
+        /**
+         * Get browser language(s)
+         *
+         * This method is based on code you may find here:
+         * http://aktuell.de.selfhtml.org/artikel/php/httpsprache/
+         *
+         * @access public
+         * @param  boolean $strict_mode
+         * @return string
+         **/
+        public static function getfrombrowser( $strict_mode = true )
+        {
+            $browser_langs = array();
+            $lang_variable = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+
+            if ( empty( $lang_variable ) )
+                return self::$defaults[ 'default' ];
+
+            $accepted_languages = preg_split( '/,\s*/', $lang_variable );
+            $current_q          = 0;
+
+            foreach ( $accepted_languages as $accepted_language )
+            {
+                // match valid language entries
+                $res = preg_match( '/^([a-z]{1,8}(?:-[a-z]{1,8})*)(?:;\s*q=(0(?:\.[0-9]{1,3})?|1(?:\.0{1,3})?))?$/i', $accepted_language, $matches );
+                // invalid syntax
+                if ( !$res )
+                    continue;
+                // get language code
+                $lang_code = explode( '-', $matches[1] );
+                if ( isset( $matches[ 2 ] ) )
+                    $lang_quality = (float)$matches[2];
+                else
+                    $lang_quality = 1.0;
+
+                while ( count( $lang_code ) )
+                {
+                    $browser_langs[] = array(
+                        'lang' => strtoupper(join('-',$lang_code)),
+                        'qual' => $lang_quality
+                    );
+                    // don't use abbreviations in strict mode
+                    if ( $strict_mode )
+                        break;
+                    array_pop( $lang_code );
+                }
+            }
+
+            // order array by quality
+            $langs = wbArray::sort( $browser_langs, 'qual', 'desc', true );
+            $ret   = array();
+            foreach ( $langs as $lang )
+                $ret[] = $lang[ 'lang' ];
+
+            return $ret;
+
+        } // end function getfrombrowser()
+
+        /**
+         *
+         *
+         *
+         *
+         **/
+        public static function addFile($file,$var=NULL)
+        {
+            $check_var = 'LANG';
+            if ( isset( $var ) )
+            {
+                $var = str_ireplace( '$', '', $var );
+                eval( 'global $' . $var . ';' );
+                eval( "\$lang_var = & \$$var;" );
+                $check_var = $var;
+            }
+            if(pathinfo($file,PATHINFO_EXTENSION)=='')
+                $file .= '.php';
+            foreach( self::$defaults['paths'] as $path )
+            {
+                if(file_exists(wbValidate::path($path.'/'.$file)))
+                {
+                    self::log(sprintf('found language file [%s] in path [%s]',$file,$path),7);
+                    self::checkFile( wbValidate::path($path.'/'.$file), $check_var );
+                    return true;
+                }
+            }
+            self::log(sprintf('language file [%s] not found!',$file),3);
+        } // end function addFile ()
+
+        /**
+         * set language file path
+         *
+         * @access public
+         * @param  string   $path  - existing directory
+         * @return void
+         **/
+        public static function addPath($path,$var=NULL)
+        {
+            $path = wbValidate::path($path);
+            if ( file_exists($path) && is_dir($path) )
+            {
+                self::log(sprintf('setting language path to [%s]',$path),7);
+                if(!in_array($path,self::$defaults['paths']))
+                    array_push(self::$defaults['paths'],$path);
+            }
+            else
+            {
+                // we don't throw an exception here, just drop a message to the
+                // error log
+                self::log(sprintf('not found (or not a directory): [%s]',$path),2);
+            }
+        } // end function addPath()
+
+        /**
+         * check for valid language file
+         *
+         * @access public
+         * @param  string  $file
+         * @param  string  $check_var
+         * @param  boolean $check_only
+         * @return boolean
+         **/
+        public static function checkFile( $file, $check_var, $check_only = false )
+        {
+            try
+            {
+                // require the language file
+                @require( $file );
+                // check if the var is defined now
+                if ( isset( ${$check_var} ) )
+                {
+                    $isIndexed = array_values( ${$check_var} ) === ${$check_var};
+                    if ( $isIndexed )
+                        return false;
+
+                    if ( $check_only )
+                    {
+                        return ${$check_var};
+                    }
+                    else
+                    {
+                        // this will replace any keys already loaded!
+                        self::$_strings = array_merge( self::$_strings, ${$check_var} );
+                        if ( preg_match( "/(\w+)\.php/", $file, $matches ) )
+                            self::$_current = $matches[1];
+                        self::$_loaded[$file] = 1;
+                        self::log(sprintf('loaded language file [%s]',$file),7);
+                        return true;
+                    }
+                }
+                else
+                {
+                    self::log(sprintf('invalid lang file [%s]',$file),2);
+                    return false;
+                }
+            }
+            catch( wbLangException $e )
+            {
+            }
+        }   // end function checkFile()
+
+    	/**
+    	 *
+    	 *
+    	 *
+    	 *
+    	 **/
+    	public function isLoaded( $file )
+    	{
+    	    if ( isset( self::$_loaded[$file] ) )
+    	        return true;
+    		return false;
+    	}   // end function isLoaded()
+
+        /**
+         * accessor to Analog (if installed)
+         *
+         * Note: Log messages are ignored if no Analog is available!
+         *
+         * @access private
+         * @param  string   $message
+         * @param  integer  $level
+         * @return
+         **/
+        private static function log($message, $level = 3)
+        {
+            if($level<>self::$loglevel) return;
+            if( !self::$analog && !self::$analog == -1)
+            {
+                if(file_exists(dirname(__FILE__).'/3rdparty/Analog/wblib.init.inc.php'))
+                {
+                    include_once(dirname(__FILE__).'/3rdparty/Analog/wblib.init.inc.php');
+                    wblib_init_3rdparty(dirname(__FILE__).'/debug/','wbLang',self::$loglevel);
+                    self::$analog = true;
+                }
+                else
+                {
+                    self::$analog = -1;
+                }
+            }
+            if ( self::$analog )
+                \Analog::log($message,$level);
+        }   // end function log()
+        
+    }
+
+    /**
+     * array helper methods
+     *
+     * @category   wblib
+     * @package    wbArray
+     * @copyright  Copyright (c) 2013 BlackBird Webprogrammierung
+     * @license    GNU LESSER GENERAL PUBLIC LICENSE Version 3
+     */
+    if ( ! class_exists( 'wbArray', false ) )
+    {
+        class wbArray
+        {
+            /**
+             * Found here:
+             * http://www.php.net/manual/en/function.array-change-key-case.php#107715
+             **/
+            public static function array_change_key_case_unicode($arr, $c = CASE_LOWER) {
+                $c = ($c == CASE_LOWER) ? MB_CASE_LOWER : MB_CASE_UPPER;
+                foreach ($arr as $k => $v) {
+                    $ret[mb_convert_case($k, $c, "UTF-8")] = $v;
+                }
+                return $ret;
+            }   // end function array_change_key_case_unicode()
+
+            /**
+             * sort an array
+             *
+             *
+             *
+             **/
+            public static function sort ( $array, $index, $order='asc', $natsort=FALSE, $case_sensitive=FALSE )
+            {
+                if( is_array($array) && count($array)>0 ) {
+                     foreach(array_keys($array) as $key) {
+                         $temp[$key]=$array[$key][$index];
+                     }
+                     if(!$natsort) {
+                         ($order=='asc')? asort($temp) : arsort($temp);
+                     }
+                     else {
+                         ($case_sensitive)? natsort($temp) : natcasesort($temp);
+                         if($order!='asc') {
+                             $temp=array_reverse($temp,TRUE);
+                         }
+                     }
+
+                     foreach(array_keys($temp) as $key) {
+                         (is_numeric($key))? $sorted[]=$array[$key] : $sorted[$key]=$array[$key];
+                     }
+                     return $sorted;
+                }
+                return $array;
+            }   // end function sort()
+
+        }
+    }
+
+    /**
+     * validation helper methods
+     *
+     * @category   wblib
+     * @package    wbValidate
+     * @copyright  Copyright (c) 2013 BlackBird Webprogrammierung
+     * @license    GNU LESSER GENERAL PUBLIC LICENSE Version 3
+     */
+    if ( ! class_exists( 'wbValidate', false ) )
+    {
+        class wbValidate
+        {
+              /**
+             * fixes a path by removing //, /../ and other things
+             *
+             * @access public
+             * @param  string  $path - path to fix
+             * @return string
+             **/
+            public static function path( $path )
+            {
+                // remove / at end of string; this will make sanitizePath fail otherwise!
+                $path       = preg_replace( '~/{1,}$~', '', $path );
+                // make all slashes forward
+                $path       = str_replace( '\\', '/', $path );
+                // bla/./bloo ==> bla/bloo
+                $path       = preg_replace('~/\./~', '/', $path);
+                // resolve /../
+                // loop through all the parts, popping whenever there's a .., pushing otherwise.
+                $parts      = array();
+                foreach ( explode('/', preg_replace('~/+~', '/', $path)) as $part )
+                {
+                    if ($part === ".." || $part == '')
+                        array_pop($parts);
+                    elseif ($part!="")
+                        $parts[] = $part;
+                }
+                $new_path = implode("/", $parts);
+                // windows
+                if ( ! preg_match( '/^[a-z]\:/i', $new_path ) )
+                    $new_path = '/' . $new_path;
+                return $new_path;
+            }   // end function path()
+        }
+    }
+
+    class wbLangException extends \Exception {}
+
+}
+
